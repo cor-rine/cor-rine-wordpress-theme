1e9b3d21c625a2c82d677dd29eff791b1d77c52e
ctoracchio <corrine.is@gmail.com>
2014-01-18 17:02:28 -0800

    Updates to the footer sdk.dir=/Users/ctoracchio/android/sdk

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '91dde3e90c0f52027bfa715944181aa3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '81534ec75338a444deda0e1ed34b68c8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
