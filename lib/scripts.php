a1a608dd8ee1e18336433061b71b0e0bfc3e3d54
ctoracchio <corrine.is@gmail.com>
2014-01-19 18:39:15 -0800

    Footer complete

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b634dc75fcce2c6803aaae3e67267e81');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5d40ef0a8d50e2834c7eeb87ddb5f2b6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -2,43 +2,71 @@
     <div class="row">
 
 
-        <div class="col-sm-6">
+        <div class="col-md-4">
 
-            <div class="archives">
-                <h4>Archives</h4>
+            <div class="box recent-posts">
+                <h4>Recent Posts</h4>
                 <ul>
-                    <?php wp_get_archives( array( 'type' => 'monthly', 'limit' => 12 ) ); ?>
-                    <li><a href="">June 2015</a></li>
-                    <li><a href="">June 2015</a></li>
-                    <li><a href="">June 2015</a></li>
-                    <li><a href="">June 2015</a></li>
+                    <?php
+                        $args = array( 'numberposts' => '10' );
+                        $recent_posts = wp_get_recent_posts( $args );
+                        foreach( $recent_posts as $recent ){
+                            echo '<li><a href="' . get_permalink($recent["ID"]) . '"><span class="icon icon-'.get_post_format($recent["ID"]).'"></span>' .   $recent["post_title"].'</a> </li> ';
+                        }
+                    ?>
                 </ul>
             </div>
 
-            <div class="social">
-                <h4>Connect/Creep</h4>
+        </div>
+
+
+        <div class="col-md-4">
+            <div class="box archives">
+                <h4>Archives</h4>
+                <ul>
+                    <?php wp_get_archives( array( 'type' => 'monthly', 'limit' => 12 ) ); ?>
+                </ul>
+            </div>
+
+            <div class="box categories">
+                <h4>Categories</h4>
+                <ul>
+                    <?php
+                        $args = array(
+                          'orderby' => 'name',
+                          'parent' => 0
+                          );
+                        $categories = get_categories( $args );
+                        foreach ( $categories as $category ) {
+                            echo '<li><a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="col-md-4">
+            <div class="box searchbar">
+                <?php get_search_form(); ?>
+            </div>
+
+            <div class="box social">
+                <h4>Connect/Creep Corrine</h4>
                 <a href="https://www.facebook.com/corrinetoracchio" class="icon icon-facebook" target="_blank"></a>
                 <a href="http://instagram.com/cor_rines" class="icon icon-instagram" target="_blank"></a>
                 <a href="https://twitter.com/cor_rine" class="icon icon-twitter" target="_blank"></a>
                 <a href="http://www.linkedin.com/pub/corrine-toracchio/41/668/513" class="icon icon-linkedin" target="_blank"></a>
-                
             </div>
             
-
         </div>
-
-        <div class="col-sm-6">
-            <?php get_search_form(); ?>
-        </div>
-
-
         
 
     </div>
 
-    <div class="row">
+    <div class="row copyright">
         <div class="col-lg-12">
-            <p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+            <p class="copyright">&copy; <?php echo date('Y'); ?> Corrine Toracchio</p>
         </div>
     </div>
 
