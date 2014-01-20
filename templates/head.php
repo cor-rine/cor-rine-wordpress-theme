c04d22a8ff3059634dc39ad54642b0a92942433c
ctoracchio <corrine.is@gmail.com>
2014-01-20 00:13:50 -0800

    Fixed facebook meta stuffs

--- templates/head.php
+++ templates/head.php
@@ -7,10 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Crimson+Text|Carrois+Gothic+SC' rel='stylesheet' type='text/css'>
   
-  <?php wp_head();
+    <meta property="og:title" content="cortravels | <?php echo the_title(); ?>"/>
+    <meta property="og:image" content="http://cor-rine.com/blog/wp-content/themes/cor-rine-wordpress-theme/assets/img/cor-travel.jpg"/>
+    <meta property="og:url" content="<?php echo get_permalink( $post->ID ); ?>"/>
+    <meta property="og:site_name" content="cortravels"/>
+    <meta property="og:type" content="blog"/>
 
-  include "lib/og.php";
-  ?>
+  <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
   
