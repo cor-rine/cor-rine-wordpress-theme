9de980b7d37d42be8ab35a7c56f9cb11492bfd3a
cor-rine <corrine.is@gmail.com>
2014-02-24 21:02:02 -0800

    fixed image width in post

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dc25194221c3ecb0dde0250f72e85e72');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '72f33c6a7fcf71821e62444d56460f5b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
