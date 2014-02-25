198bdc6791ccb32ff5d542905c67b09f2f5c3296
cor-rine <corrine.is@gmail.com>
2014-02-24 21:06:54 -0800

    fixed figure tag

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '72f33c6a7fcf71821e62444d56460f5b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'baf4e8e4f0365ce1dc1f90abbedbdfb3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
