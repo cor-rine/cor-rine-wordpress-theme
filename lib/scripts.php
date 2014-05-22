725a3132ac09b42b5c9fa40ab375d08ecf37e67d
cor-rine <corrine.is@gmail.com>
2014-05-22 21:37:03 +0530

    fixed styling of a divider

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'abd4ce90c8c28d0e415420b36c7e1209');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3fca13095d160103262b552cac2bb736');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
