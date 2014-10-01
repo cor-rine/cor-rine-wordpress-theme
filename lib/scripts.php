252941779c9247f1c35b6e25bb786c897c246e08
cor-rine <corrine.is@gmail.com>
2014-10-01 11:27:36 +0700

    Fixed social styles again

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '068c1c44d87c30568c1ece2a8c792ef3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4227982ab37a268c71c24306adc25f78');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
